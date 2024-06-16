8230a795ba3dbcb7ded85409de7d848b583fd2ec
Thanh3102 <thanhduong200602@gmail.com>
2024-06-16 12:35:16 +0700

    Update date format

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,3 +1,9 @@
 export const formatDate = (date: Date) => {
-  return date.toString().slice(0, 19).replace(/-/g, "/").replace("T", " ");
+  const [d, t] = date.toString().split("T");
+  const [yy, mm, dd] = d.split("-");
+  const time = t.slice(0, 8);
+
+  return `${dd}/${mm}/${yy} ${time}`;
+
+  // return date.toString().slice(0, 19).replace(/-/g, "/").replace("T", " ");
 };
