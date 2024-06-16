d1282f4e23f2e8a56af478c884753ca53bc1d67d
Thanh3102 <thanhduong200602@gmail.com>
2024-06-16 12:34:10 +0700

    Update app.tsx and index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,83 @@
-import React, { ReactElement } from "react";
+import React, { Fragment, ReactElement } from "react";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  ArcElement,
+} from "chart.js";
 import Login from "./components/page/Login";
 import Home from "./components/page/Home";
 import ProtectedRoute from "./components/feature/ProtectedRoute";
 import Warehouse from "./components/page/Warehouse";
 import Product from "./components/page/Product";
+import Definition from "./components/page/Definition";
+import Order from "./components/page/Order";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  ArcElement,
+  Filler,
+  Title,
+  Tooltip,
+  Legend
+);
+
+type RouteType = { path: string; element: ReactElement; protected: boolean };
 
 function App() {
-  const routes: { path: string; element: ReactElement; protected: boolean }[] =
-    [
-      {
-        path: "/",
-        element: <Home />,
-        protected: true,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-        protected: false,
-      },
-      {
-        path: "/warehouse",
-        element: <Warehouse />,
-        protected: true,
-      },
-      {
-        path: "product",
-        element: <Product />,
-        protected: true,
-      },
-    ];
+  const routes: RouteType[] = [
+    {
+      path: "/",
+      element: <Home />,
+      protected: true,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+      protected: false,
+    },
+    {
+      path: "/warehouse",
+      element: <Warehouse />,
+      protected: true,
+    },
+    {
+      path: "/product",
+      element: <Product />,
+      protected: true,
+    },
+    {
+      path: "/definition",
+      element: <Definition />,
+      protected: true,
+    },
+    {
+      path: "/order",
+      element: <Order />,
+      protected: true,
+    },
+    // {
+    //   path: "/service",
+    //   element: <Service />,
+    //   protected: true,
+    // },
+  ];
 
   return (
     <div className="App">
-      <ToastContainer autoClose={2000} hideProgressBar={true} />
+      <ToastContainer autoClose={2000} hideProgressBar={true} limit={1} />
       <Routes>
         {routes.map((route, index) => {
           return (
