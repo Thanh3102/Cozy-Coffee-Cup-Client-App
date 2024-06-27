03b0dc88f27c0aac54774431b6a7ba528111f4ec
Thanh3102 <thanhduong200602@gmail.com>
2024-06-27 15:40:18 +0700

    Add permission required component and update ProtectedRoute component

---
+++ src/components/feature/PermissionRequire.tsx
@@ -0,0 +1,36 @@
+import { Fragment, useEffect, useState } from "react";
+import { useAppSelector } from "../../redux/store";
+import { Permission } from "../../utils/types/enum";
+import { BaseProps } from "../../utils/types/interface";
+import AccountApi from "../../api/Account";
+import DeniedAccess from "../page/DeniedAccess";
+
+interface Props extends BaseProps {
+  require: Permission;
+}
+
+const PermissionRequire = ({ require, children }: Props) => {
+  const { user } = useAppSelector((state) => state.auth);
+  const [permissions, setPermissions] = useState<Permission[]>();
+  const getUserPermission = async () => {
+    if (user) {
+      const accountApi = new AccountApi();
+      const perms = await accountApi.getAccountPermissions(user.id);
+      setPermissions(perms);
+    }
+  };
+
+  useEffect(() => {
+    getUserPermission();
+  }, []);
+
+  if (permissions) {
+    if (permissions.includes(require)) {
+      return <Fragment>{children}</Fragment>;
+    }
+  }
+
+  return <DeniedAccess />;
+};
+
+export default PermissionRequire;
